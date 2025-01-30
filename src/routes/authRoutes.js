3607c9eacc667395f77bdf42a789688e9293031c
Khayrullo Isomiddinov <isomiddinovkhayrullo@gmail.com>
2025-01-30 17:48:29 +0100

    Fixed authMiddleware path issue by adding src/

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { signup, login, getUser } = require("../controllers/authController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../src/middleware/authMiddleware");
 
 const router = express.Router();
 
